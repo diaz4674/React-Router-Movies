df5a2b164c2e6fd311286f83fe5cf747b9d61bc6
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-03-30 11:28:20 -0700

    Refactored to DRY code for 1st stretch

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -36,7 +36,6 @@ export default class MovieList extends Component {
 function MovieDetails({ movie }) {
   return (
   <div className = 'movie-list'>
-   <NavLink to = {`/movies/${movie.id}` } > <MovieCard movie = {movie} />  </NavLink> 
-  
+    <NavLink to = {`/movies/${movie.id}`} style={{textDecoration: 'none', color: "black"}} > <MovieCard movie = {movie} />  </NavLink> 
   </div>)
 }
